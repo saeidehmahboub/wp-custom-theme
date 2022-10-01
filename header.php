ddce3c2ea2b8010b68849c6ede3c9dafae330c7c
Saeideh Mahboub <saeideh.mahboub@gmail.com>
2022-10-01 16:42:07 +0400

    Changed text domain based on itineris check

--- header.php
+++ header.php
@@ -9,6 +9,6 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'iwct'); ?></a>
+    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'my-theme'); ?></a>
 
     <div id="primary">
