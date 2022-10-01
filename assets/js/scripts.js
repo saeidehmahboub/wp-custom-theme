aa3cb256ed624ca283044ee4375c7eef8d732790
Saeideh Mahboub <saeideh.mahboub@gmail.com>
2022-10-01 22:47:16 +0400

    Develop filters

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,20 @@
 (function ($) {
   // Document is Ready!
-  $(function () {});
+  $(function () {
+    $(".iwct-filters-button").click(function () {
+      const button = $(this);
+      const buttonIcon = button.children("i");
+      const isButtonUp = buttonIcon.hasClass("fa-chevron-up");
+      const filtersWrapper = button.parent().next();
+
+      const buttonClassToggle = (removeClass, addClass) =>
+        buttonIcon.removeClass(removeClass).addClass(addClass);
+
+      isButtonUp
+        ? buttonClassToggle("fa-chevron-up", "fa-chevron-down")
+        : buttonClassToggle("fa-chevron-down", "fa-chevron-up");
+
+      filtersWrapper.slideToggle();
+    });
+  });
 })(jQuery);
